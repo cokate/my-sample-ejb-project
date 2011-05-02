fc9014d853aaca1d8038c826312b2ec7de288007
raffaele.arecchi <raffaele.arecchi@gmail.com>
2011-05-02 11:07:41 +0000

    first commit

---
+++ StatefulSessionWeb2/trunk/src/com/ivan/scbcd6/StatefulSession2Local.java
@@ -0,0 +1,15 @@
+package com.ivan.scbcd6;
+
+import javax.ejb.Local;
+
+@Local
+public interface StatefulSession2Local {
+	/**
+	 * Creates a greeting to the person with the supplied name.
+	 * 
+	 * @param inName
+	 *            Name of person to greet.
+	 * @return Greeting.
+	 */
+	public String greeting(final String inName);
+}
