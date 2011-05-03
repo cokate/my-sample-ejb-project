4c058a5c812e620f67666ade4f289117a90d2f2f
raffaele.arecchi@gmail.com <raffaele.arecchi@gmail.com@38292517-384e-58d8-c5fa-7c8c813b7480>
2011-05-03 09:18:16 +0000

    first commit

---
+++ StatelessSession4Web/trunk/src/com/ivan/scbcd6/StatelessSession4Bean.java
@@ -0,0 +1,31 @@
+package com.ivan.scbcd6;
+
+import java.util.Date;
+
+import javax.annotation.PostConstruct;
+
+public class StatelessSession4Bean {
+	private static int sCurrentInstanceNumber = 1;
+	private int mInstanceNumber;
+
+	@PostConstruct
+	public void initialize() {
+		mInstanceNumber = sCurrentInstanceNumber++;
+		System.out.println("*** StatelessSession4Bean " + mInstanceNumber + " created.");
+	}
+
+	/**
+	 * Creates a greeting to the person with the supplied name.
+	 * 
+	 * @param inName
+	 *            Name of person to greet.
+	 * @return Greeting.
+	 */
+	public String greeting(final String inName) {
+		Date theCurrentTime = new Date();
+		String theMessage = "Hello " + inName + ", I am stateless session bean " + mInstanceNumber + ". The time is now: "
+				+ theCurrentTime;
+		return theMessage;
+	}
+
+}
