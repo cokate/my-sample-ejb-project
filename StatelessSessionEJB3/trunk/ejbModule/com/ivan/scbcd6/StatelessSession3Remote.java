37f11be33009caab042db1fc2ab6dd9478a7dd74
raffaele.arecchi <raffaele.arecchi@gmail.com>
2011-05-03 09:08:40 +0000

    first commit

---
+++ StatelessSessionEJB3/trunk/ejbModule/com/ivan/scbcd6/StatelessSession3Remote.java
@@ -0,0 +1,16 @@
+package com.ivan.scbcd6;
+
+import javax.ejb.Remote;
+
+@Remote
+public interface StatelessSession3Remote {
+	/**
+	 * Creates a greeting to the person with the supplied name.
+	 * 
+	 * @param inName
+	 *            Name of person to greet.
+	 * @return Greeting.
+	 */
+	public String greeting(final String inName);
+
+}
