5101543b7853e6d3b8e6df95a1b8e760c56e5034
raffaele.arecchi <raffaele.arecchi@gmail.com>
2011-05-05 08:59:11 +0000

    first commit

---
+++ SingletonSessionBeanConcurrency/trunk/src/com/ivan/scbcd6/SingletonSessionBeanB.java
@@ -0,0 +1,35 @@
+package com.ivan.scbcd6;
+
+import javax.ejb.ConcurrencyManagement;
+import javax.ejb.ConcurrencyManagementType;
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+
+/**
+ * This class implements a singleton session bean with bean-managed concurrency.
+ */
+
+@Singleton
+@LocalBean
+@ConcurrencyManagement(ConcurrencyManagementType.BEAN)
+public class SingletonSessionBeanB {
+	public void slowMethod() {
+		System.out.println("SingletonSessionBeanB - Entering slowMethod");
+		waitSomeTime(10);
+		System.out.println("SingletonSessionBeanB - Exiting slowMethod");
+	}
+
+	public void fastMethod() {
+		System.out.println("SingletonSessionBeanB - Entering fastMethod");
+		waitSomeTime(1);
+		System.out.println("SingletonSessionBeanB - Exiting fastMethod");
+	}
+
+	private void waitSomeTime(final long inSecondsDelay) {
+		try {
+			Thread.sleep(1000L * inSecondsDelay);
+		} catch (InterruptedException e) {
+			// Ignore exceptions.
+		}
+	}
+}
