8daaaae6d73f9979b6d4db261325e2764479496f
raffaele.arecchi <raffaele.arecchi@gmail.com>
2011-05-05 10:02:05 +0000

    update for concurrent

--- SingletonSessionBeanConcurrency/trunk/src/com/ivan/scbcd6/SingletonSessionBeanA.java
+++ SingletonSessionBeanConcurrency/trunk/src/com/ivan/scbcd6/SingletonSessionBeanA.java
@@ -1,8 +1,11 @@
 package com.ivan.scbcd6;
 
+import javax.ejb.AccessTimeout;
 import javax.ejb.ConcurrencyManagement;
 import javax.ejb.ConcurrencyManagementType;
 import javax.ejb.LocalBean;
+import javax.ejb.Lock;
+import javax.ejb.LockType;
 import javax.ejb.Singleton;
 
 /**
@@ -12,13 +15,16 @@ import javax.ejb.Singleton;
 @Singleton
 @LocalBean
 @ConcurrencyManagement(ConcurrencyManagementType.CONTAINER)
+@Lock(LockType.WRITE)
 public class SingletonSessionBeanA {
+
 	public void slowMethod() {
 		System.out.println("SingletonSessionBeanA - Entering slowMethod");
 		waitSomeTime(10);
 		System.out.println("SingletonSessionBeanA - Exiting slowMethod");
 	}
-
+	
+	@AccessTimeout(100)
 	public void fastMethod() {
 		System.out.println("SingletonSessionBeanA - Entering fastMethod");
 		waitSomeTime(1);
