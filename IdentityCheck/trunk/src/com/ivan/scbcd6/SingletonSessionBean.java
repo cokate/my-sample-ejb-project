2e559547b8604faa065737c59a6dcdcf34f7b65f
raffaele.arecchi <raffaele.arecchi@gmail.com>
2011-05-07 10:20:34 +0000

    first commit

---
+++ IdentityCheck/trunk/src/com/ivan/scbcd6/SingletonSessionBean.java
@@ -0,0 +1,18 @@
+package com.ivan.scbcd6;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+
+@Singleton
+@LocalBean
+public class SingletonSessionBean {
+	public SingletonSessionBean() {
+		System.out.println("***** SingletonSessionBean created");
+	}
+
+	@PostConstruct
+	public void initialize() {
+		System.out.println("***** SingletonSessionBean initialized");
+	}
+}
---
+++ IdentityCheck/trunk/src/com/ivan/scbcd6/StatelessSessionBeanLocalBusiness.java
@@ -0,0 +1,8 @@
+package com.ivan.scbcd6;
+
+import javax.ejb.Local;
+
+@Local
+public interface StatelessSessionBeanLocalBusiness {
+
+}
