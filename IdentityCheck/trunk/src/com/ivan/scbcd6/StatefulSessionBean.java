d672df84dd4df1a461054e6e3963990090524373
raffaele.arecchi@gmail.com <raffaele.arecchi@gmail.com@38292517-384e-58d8-c5fa-7c8c813b7480>
2011-05-07 10:20:34 +0000

    first commit

---
+++ IdentityCheck/trunk/src/com/ivan/scbcd6/StatefulSessionBean.java
@@ -0,0 +1,18 @@
+package com.ivan.scbcd6;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateful;
+
+@Stateful
+@LocalBean
+public class StatefulSessionBean {
+	public StatefulSessionBean() {
+		System.out.println("***** StatefulSessionBean created");
+	}
+
+	@PostConstruct
+	public void initialize() {
+		System.out.println("***** StatefulSessionBean initialized");
+	}
+}
---
+++ IdentityCheck/trunk/src/com/ivan/scbcd6/StatelessSessionBean.java
@@ -0,0 +1,18 @@
+package com.ivan.scbcd6;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+@Stateless
+@LocalBean
+public class StatelessSessionBean implements StatelessSessionBeanLocalBusiness {
+	public StatelessSessionBean() {
+		System.out.println("***** StatelessSessionBean created");
+	}
+
+	@PostConstruct
+	public void initialize() {
+		System.out.println("***** StatelessSessionBean initialized");
+	}
+}
